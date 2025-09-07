280fadc9936489ff9c92557e0ec6164b66d39a44
lx-Meteors <1762204162@qq.com>
2025-09-07 19:24:18 +0800

    .

--- path_config.py
+++ path_config.py
@@ -1,5 +1,6 @@
 
 # this is the RARC-KVResidual-to-KVCache branch
 # the status is wo-HiddenState, only role,mask and KVCache
+# test: another matebook
 import os
 BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
