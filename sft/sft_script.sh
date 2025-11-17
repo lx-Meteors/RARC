3830ac89261c66e3a9e2c57d35e77975f3b49112
lx-Meteors <1762204162@qq.com>
2025-11-17 19:58:28 +0800

    500xCompress_Summary

--- sft/sft_script.sh
+++ sft/sft_script.sh
@@ -24,7 +24,7 @@
 # python ../util/evaluate_ood.py --work_dir '../experiment/local_experiment/ICAE_Llama-3.2-1B_DPL'
 # python ../util/evaluate_iid.py --work_dir '../experiment/local_experiment/ICAE_Llama-3.2-1B_DPL'
 
-#python instruction_prepare_data.py --work_dir  '../experiment/main_experiment/ICAE_EPL_1B_SingleChunk'
+#python instruction_prepare_data.py --work_dir  '../experiment/rebuttal/15x_8gpu/SAC_qmsum_govreport'
 CUDA_VISIBLE_DEVICES=0,1,2,3 nohup python ./instruction_trainer.py --work_dir   '../experiment/main_experiment/ICAE_EPL_1B_SingleChunk' --port 14527 > train.log 2>&1 &
 CUDA_VISIBLE_DEVICES=0,1,2,3 python ./instruction_evaluator.py --work_dir   '../experiment/main_experiment/ICAE_EPL_1B_SingleChunk' --batch_size 1
 python ../util/evaluate_ood.py --work_dir  '../experiment/main_experiment/500x_DPL_1B_MultiChunk'
@@ -37,3 +37,4 @@ CUDA_VISIBLE_DEVICES=1,2,3,4 python ./instruction_evaluator.py --work_dir   '../
 #nohup python ./instruction_trainer.py --work_dir   '../experiment/main_experiment/ICAE_EPL_1B_SingleChunk' --port 14527 > train.log 2>&1 &
 # tail -f train.log
 
+/mnt/zhaorunsong/lx/RARC/experiment/rebuttal/15x_8gpu/SAC_qmsum_govreport
